b725f98c63ce98f438ea6af062c213d01e556ea3
Syd McGee <sydmcgee@gmail.com>
2013-10-29 15:00:13 -0700

    Who can Who can't

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,6 +2,7 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
+  
 
   def index
     @pins = Pin.all
@@ -41,14 +42,14 @@ class PinsController < ApplicationController
 
    private
     # Use callbacks to share common setup or constraints between actions.
-    def set_pin
+  def set_pin
       @pin = Pin.find(params[:id])
-    end
+  end
 
-    def correct_user
-      @pin = current_user.pin.find_by(id: params[:id])
-      redirect_to pins_path, notice: 'Not authorized to edit this pin' if @pin.nil?
-    end
+  def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+  end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
